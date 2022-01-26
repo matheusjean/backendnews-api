5fa71804335d4d67a6e6714d8828509d01ef04e1
matheusjean <matheusjean11@gmail.com>
2022-01-26 15:08:35 -0300

    fix: remove redis, add async/await on controller

--- dist/src/controller/newsController.js
+++ dist/src/controller/newsController.js
@@ -1,51 +1,71 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const newsService_1 = require("../services/newsService");
 const HttpStatus = require("http-status");
 const helper_1 = require("../infra/helper");
-const redis = require("redis");
 class NewsController {
     get(req, res) {
-        let client = redis.createClient();
-        client.get('news', (err, reply) => {
-            if (reply) {
-                console.log('redis');
-                helper_1.default.sendResponse(res, HttpStatus.OK, JSON.parse(reply));
-            }
-            else {
-                newsService_1.default.get()
-                    .then((news) => {
-                    client.set('news', JSON.stringify(news));
-                    helper_1.default.sendResponse(res, HttpStatus.OK, news);
-                })
-                    .catch((error) => console.error.bind(console, `Error${error}`));
-            }
-        });
-    }
-    getById(req, res) {
-        const _id = req.params.id;
-        newsService_1.default.getById(_id)
+        newsService_1.default.get()
             .then((news) => helper_1.default.sendResponse(res, HttpStatus.OK, news))
             .catch((error) => console.error.bind(console, `Error${error}`));
     }
+    getById(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const _id = req.params.id;
+                let response = yield newsService_1.default.getById(_id);
+                helper_1.default.sendResponse(res, HttpStatus.OK, response);
+            }
+            catch (error) {
+                console.error(error);
+            }
+        });
+    }
     create(req, res) {
-        let vm = req.body;
-        newsService_1.default.create(vm)
-            .then((news) => helper_1.default.sendResponse(res, HttpStatus.OK, 'Noticia cadastrada com sucesso!'))
-            .catch((error) => console.error.bind(console, `Error${error}`));
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                let vm = req.body;
+                yield newsService_1.default.create(vm);
+                helper_1.default.sendResponse(res, HttpStatus.OK, 'Noticia cadastrada com sucesso!');
+            }
+            catch (error) {
+                console.error(error);
+            }
+        });
     }
     update(req, res) {
-        const _id = req.params.id;
-        let news = req.body;
-        newsService_1.default.update(_id, news)
-            .then((news) => helper_1.default.sendResponse(res, HttpStatus.OK, `Notícia foi atualizada com sucesso`))
-            .catch((error) => console.error.bind(console, `Error${error}`));
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const _id = req.params.id;
+                let news = req.body;
+                yield newsService_1.default.update(_id, news);
+                helper_1.default.sendResponse(res, HttpStatus.OK, `Noticia atualiza com sucesso!`);
+            }
+            catch (error) {
+                console.error(error);
+            }
+        });
     }
     delete(req, res) {
-        const _id = req.params.id;
-        newsService_1.default.delete(_id)
-            .then(() => helper_1.default.sendResponse(res, HttpStatus.OK, `Noticia deletada com sucesso`))
-            .catch((error) => console.error.bind(console, `Error${error}`));
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const _id = req.params.id;
+                yield newsService_1.default.delete(_id);
+                helper_1.default.sendResponse(res, HttpStatus.OK, 'Noticia deletada com sucesso!');
+            }
+            catch (error) {
+                console.error(error);
+            }
+        });
     }
 }
 exports.default = new NewsController();
--- src/controller/newsController.ts
+++ src/controller/newsController.ts
@@ -1,71 +1,57 @@
 import NewsService from '../services/newsService';
 import * as HttpStatus from 'http-status';
 import Helper from '../infra/helper';
-import * as redis from 'redis';
 
 class NewsController {
-  get(req, res) {
-    let client = redis.createClient();
-
-    client.get('news', (err, reply) => {
-      if (reply) {
-        Helper.sendResponse(res, HttpStatus.OK, JSON.parse(reply));
-      } else {
-        NewsService.get()
-          .then((news) => {
-            client.set('news', JSON.stringify(news));
-            Helper.sendResponse(res, HttpStatus.OK, news);
-          })
-          .catch((error) => console.error.bind(console, `Error${error}`));
-      }
-    });
-  }
-
-  getById(req, res) {
-    const _id = req.params.id;
-
-    NewsService.getById(_id)
+  async get(req, res) {
+    await NewsService.get()
       .then((news) => Helper.sendResponse(res, HttpStatus.OK, news))
       .catch((error) => console.error.bind(console, `Error${error}`));
   }
 
-  create(req, res) {
-    let vm = req.body;
-
-    NewsService.create(vm)
-      .then((news) =>
-        Helper.sendResponse(
-          res,
-          HttpStatus.OK,
-          'Noticia cadastrada com sucesso!',
-        ),
-      )
-      .catch((error) => console.error.bind(console, `Error${error}`));
+  async getById(req, res) {
+    try {
+      const _id = req.params.id;
+      let response = await NewsService.getById(_id);
+      Helper.sendResponse(res, HttpStatus.OK, response);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  update(req, res) {
-    const _id = req.params.id;
-    let news = req.body;
-
-    NewsService.update(_id, news)
-      .then((news) =>
-        Helper.sendResponse(
-          res,
-          HttpStatus.OK,
-          `Notícia foi atualizada com sucesso`,
-        ),
-      )
-      .catch((error) => console.error.bind(console, `Error${error}`));
+  async create(req, res) {
+    try {
+      let vm = req.body;
+      await NewsService.create(vm);
+      Helper.sendResponse(
+        res,
+        HttpStatus.OK,
+        'Noticia cadastrada com sucesso!',
+      );
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  delete(req, res) {
-    const _id = req.params.id;
+  async update(req, res) {
+    try {
+      const _id = req.params.id;
+      let news = req.body;
+      await NewsService.update(_id, news);
+      Helper.sendResponse(res, HttpStatus.OK, `Noticia atualiza com sucesso!`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-    NewsService.delete(_id)
-      .then(() =>
-        Helper.sendResponse(res, HttpStatus.OK, `Noticia deletada com sucesso`),
-      )
-      .catch((error) => console.error.bind(console, `Error${error}`));
+  async delete(req, res) {
+    try {
+      const _id = req.params.id;
+      await NewsService.delete(_id);
+      Helper.sendResponse(res, HttpStatus.OK, 'Noticia deletada com sucesso!');
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
 
