3ca87564069efedfe1502089c29083b9ab29de73
matheusjean <matheusjean11@gmail.com>
2022-03-23 10:44:17 -0300

    sem cors

--- startUp.ts
+++ startUp.ts
@@ -23,17 +23,17 @@ class startUp {
     this.routes();
   }
 
-  enableCors() {
-    const options: cors.CorsOptions = {
-      methods: 'GET, OPTIONS, PUT, POST, DELETE',
-      origin: '*',
-    };
+  // enableCors() {
+  //   const options: cors.CorsOptions = {
+  //     methods: 'GET, OPTIONS, PUT, POST, DELETE',
+  //     origin: '*',
+  //   };
 
-    this.app.use(cors(options));
-  }
+  //   this.app.use(cors(options));
+  // }
 
   middler() {
-    this.enableCors();
+    // this.enableCors();
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
   }
